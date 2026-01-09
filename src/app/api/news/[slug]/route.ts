d73cffe9f20987823bab68733e50031656f1f566
ceovogue <ceo@voguepools.gr>
2026-01-09 19:59:28 +0200

    Update route.ts

--- src/app/api/news/[slug]/route.ts
+++ src/app/api/news/[slug]/route.ts
@@ -2,12 +2,6 @@ export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server";
 
-export async function GET(_: Request, { params }: { params: { slug: string } }) {
-  return NextResponse.json(
-    {
-      error: "Not available",
-      slug: params.slug,
-    },
-    { status: 404 }
-  );
+export async function GET() {
+  return NextResponse.json([]);
 }
